563e972e86feb7cd70c2d66132bb4c174c50a842
Mads Jensen <mje@inducks.org>
2017-05-31 16:02:28 +0200

    template for Channel, Transport and Message

--- kombu/transport/nsq.py
+++ kombu/transport/nsq.py
@@ -4,8 +4,9 @@
 """
 from __future__ import absolute_import, unicode_literals
 
-from kombu.transport import base, virtual
-from kombu.messaging import Exchange
+import socket
+
+from kombu.transport import virtual
 
 try:
     import nsq
@@ -16,38 +17,42 @@ DEFAULT_ADMIN_PORT_SSL = 4151
 DEFAULT_PORT = 80
 DEFAULT_PORT_SSL = 443
 
+__all__ = ('Message', 'Channel', 'Transport'),
 
-class Message(nsq.Message, base.Message):
 
-    def __init__(self, id, body, timestamp, attempts):
-        super(self, base.Message).__init__(body=body)
+class Message(virtual.Message):
+    """NSQ message adapted to the AMQP protocol."""
 
-    def requeue(self):
-        return super().requeue()
-
-    def enable_async(self):
-        super().enable_async()
-
-    def finish(self):
-        super().finish()
+    def __init__(self, payload, channel=None, **kwargs):
+        super().__init__(payload, channel, **kwargs)
 
 
 class Channel(virtual.Channel):
+    """Channel for a topic in NSQ."""
+
     prefix = 'nsq'
-    Message = Message
+    Message = nsq.Message
+
+    def __init__(self, connection, **kwargs):
+        super().__init__(connection, **kwargs)
+
+    def Producer(self, *args, **kwargs):
+        # this should be nsq.Writer which
+        nsq.Writer()
+        return super().Producer(*args, **kwargs)
 
     def prepare_message(self, body, priority=None, content_type=None, content_encoding=None, headers=None,
                         properties=None):
-        return super().prepare_message(body, priority, content_type, content_encoding, headers, properties)
+        return body, properties
 
     def queue_delete(self, queue, if_unused=False, if_empty=False, **kwargs):
         super().queue_delete(queue, if_unused, if_empty, **kwargs)
 
+    def basic_cancel(self, consumer_tag):
+        super().basic_cancel(consumer_tag)
+
     def Consumer(self, *args, **kwargs):
         return super(Channel, self).Consumer(*args, **kwargs)
-
-    def __init__(self):
-        nsq.AsyncConn()
 
     @property
     def qos(self):
@@ -55,19 +60,30 @@ class Channel(virtual.Channel):
 
 
 class Transport(virtual.Transport):
+    """NSQ transport layer."""
+
     Channel = Channel
 
     default_port = DEFAULT_PORT
     driver_type = 'nsq'
     driver_name = 'nsq'
 
+    connection_errors = (
+        virtual.Transport.connection_errors + (
+            ConnectionError, socket.error
+        )
+    )
+
     def __init__(self, *args, **kwargs):
         if nsq is None:
             raise ImportError('Missing pynsq library')
 
+    def establish_connection(self):
+        return super().establish_connection()
+
+    def manager(self):
+        return super().manager()
+
     def driver_version(self):
         return nsq.__version__
 
-    def close_channel(self, channel):
-        nsq.AsyncConn.
-        super().close_channel(channel)
